8574bc3b0905dadb03985c92980fd62b163d0927
sera <j62988g@yahoo.com.tw>
2016-08-10 01:30:01 +0000

    修改時間設定方式 更改為設定時區

--- models/PageModel.php
+++ models/PageModel.php
@@ -8,63 +8,75 @@ class PageModel extends connect
         try
         {
             $this->db->beginTransaction();
-            $sql = "SELECT `total` FROM `account` WHERE `aId` = ".$_SESSION['aId']." FOR UPDATE";
-
+            $sql = "SELECT `total` FROM `account` ";
+            $sql .= "WHERE `aId` = :aId FOR UPDATE";
             $total = $this->db->prepare($sql);
-
+            $total->bindParam(':aId', $_SESSION['aId']);
             $total->execute();
             $data = $total->fetch(PDO::FETCH_ASSOC);
 
             if ($type == "轉出") {
                 if ($money > $data['total']) {
-
                     return 2;
                 } else {
                     $aId = $_SESSION['aId'];
-                    $date = date("Y-m-d H:i",time()+8*3600);
-                    $total = $data['total'] - $money;
+                    date_default_timezone_set('Asia/Taipei');
+                    $date = date("Y-m-d H:i");
+                    $sur = $data['total'] - $money;
                     $insert = $this->db->prepare("INSERT INTO `moneyDetails`".
-                    "(`aId`, `date`, `type`, `money`)".
-                    "VALUES (:aId, :date, :type, :money)");
+                    "(`aId`, `date`, `type`, `money`, `sur`)".
+                    "VALUES (:aId, :date, :type, :money, :sur)");
                     $insert->bindParam(':aId', $aId);
                     $insert->bindParam(':type', $type);
                     $insert->bindParam(':date', $date);
                     $insert->bindParam(':money', $money, PDO::PARAM_INT);
+                    $insert->bindParam(':sur', $sur, PDO::PARAM_INT);
                     $insert->execute();
-                    $total = $data['total'] - $money;
-                    $sql = "UPDATE `account` SET `total` = :total";
+
+                    $total = $sur;
+                    $sql = "UPDATE `account` SET `total` = :total ";
+                    $sql .= "WHERE `aId` = :aId";
                     $update = $this->db->prepare($sql);
                     $update->bindParam(':total', $total);
+                    $update->bindParam(':aId', $_SESSION['aId']);
                     $update->execute();
+
                     $this->db->commit();
 
                             return 3 ;
                         }
             }elseif ($type == '轉入') {
                 $aId = $_SESSION['aId'];
-                $date = date("Y-m-d H:i",time()+8*3600);
+                date_default_timezone_set('Asia/Taipei');
+                $date = date("Y-m-d H:i");
+                $sur = $data['total'] + $money;
                 $insert = $this->db->prepare("INSERT INTO `moneyDetails`".
-                "(`aId`, `date`, `type`, `money`)".
-                "VALUES (:aId, :date, :type, :money)");
+                "(`aId`, `date`, `type`, `money`, `sur`)".
+                "VALUES (:aId, :date, :type, :money, :sur)");
                 $insert->bindParam(':aId', $aId);
                 $insert->bindParam(':type', $type);
                 $insert->bindParam(':date', $date);
                 $insert->bindParam(':money', $money, PDO::PARAM_INT);
+                $insert->bindParam(':sur', $sur, PDO::PARAM_INT);
                 $insert->execute();
-                $total = $data['total'] + $money;
-                $sql = "UPDATE `account` SET `total` = :total";
+
+                $total = $sur;
+                $sql = "UPDATE `account` SET `total` = :total ";
+                $sql .= "WHERE `aId` = :aId";
                 $update = $this->db->prepare($sql);
                 $update->bindParam(':total', $total);
+                $update->bindParam(':aId', $_SESSION['aId']);
                 $update->execute();
+
                 $this->db->commit();
 
                    return 4 ;
                 }
         }catch (Exception $err)
-            {
-                $this->db->rollBack();
-                $msg = $err->getMessage();
-            }
+        {
+            $this->db->rollBack();
+            $msg = $err->getMessage();
+        }
     }
 
     public function details()
